72cfb02c523afd9c693d74cbaa8d25a69ae0fa52
phoenix122411 <phoenix122411@126.com>
2016-07-27 10:12:55 +0800

    zhangmm-bigscreen_show

---
+++ ems-chinanet-bigscreen/src/main/java/com/awifi/bigscreen_show/index/IndexShowAction.java
@@ -0,0 +1,33 @@
+package com.awifi.bigscreen_show.index;
+
+import java.util.List;
+
+import org.roof.web.dictionary.entity.Dictionary;
+import org.roof.web.dictionary.service.api.IDictionaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("awifi/bigscreen_show/indexShowAction")
+public class IndexShowAction {
+	private IDictionaryService dictionaryService;
+
+	// 加载页面的通用数据
+	private void loadCommon(Model model){
+		List<Dictionary> dicList =  dictionaryService.findByType("TEST");
+		model.addAttribute("dicList", dicList);
+	}
+
+	@RequestMapping("/index_page")
+	public String index_page() {
+		return "/awifi/bigscreen_show/index/index_page.jsp";
+	}
+
+	@Autowired(required = true)
+	public void setDictionaryService(@Qualifier("dictionaryService") IDictionaryService dictionaryService) {
+		this.dictionaryService = dictionaryService;
+	}
+}
