657327fe9b0d2c096c73007587cddaf1e47a485a
unknown <>
2016-08-28 11:35:34 +0800

    zmm-react-ems-bigscreen

--- react-ems-bigscreen/bigscreen/bigscreen.jsx
+++ react-ems-bigscreen/bigscreen/bigscreen.jsx
@@ -5,7 +5,10 @@ import ReactDOM from 'react-dom';
 
 import DynamicChartComponent from './DynamicChartComponent.jsx';
 import Line_YHRZ_ChartComponent from './ChartComponent/Line_YHRZ_ChartComponent.jsx';
+import Funnel_SBPM_ChartComponent from './ChartComponent/Funnel_SBPM_ChartComponent.jsx';
+
+
 
 ReactDOM.render(<DynamicChartComponent />, document.getElementById('funnel_SBPM'))
-
 ReactDOM.render(<Line_YHRZ_ChartComponent />, document.getElementById('Line_YHRZ_Chart'))
+ReactDOM.render(<Funnel_SBPM_ChartComponent />, document.getElementById('Funnel_SBPM_Chart'))
