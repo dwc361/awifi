c5d0b34982c807df76f91350a1a482126a80b71a
liangcong1921 <1349905198.com>
2016-10-26 10:10:25 +0800

    lc-first

--- ssmr-ems-bigscreen/src/main/webapp/react/stores/second-store.js
+++ ssmr-ems-bigscreen/src/main/webapp/react/stores/second-store.js
@@ -4,48 +4,48 @@ import $ from 'jquery';
 
 export default Reflux.createStore({
 	init () {
-       console.log("stores init.");
-       // this.listenToMany(actions); 方法前面加on，如onHandleChange
-       this.listenTo(actions.getBigscreenSecondData, 'getBigscreenSecondData');
-       this.listenTo(actions.saveBigscreenSecondData, 'saveBigscreenSecondData');
-       this.listenTo(actions.openAddModal, 'openAddModal');
-       this.listenTo(actions.setStateValue, 'setStateValue');
-	   this.listenTo(actions.getMix_Dzzd_data, 'getMix_Dzzd_data');
+		console.log("stores init.");
+		// this.listenToMany(actions); 方法前面加on，如onHandleChange
+		this.listenTo(actions.getBigscreenSecondData, 'getBigscreenSecondData');
+		this.listenTo(actions.saveBigscreenSecondData, 'saveBigscreenSecondData');
+		this.listenTo(actions.openAddModal, 'openAddModal');
+		this.listenTo(actions.setStateValue, 'setStateValue');
+		this.listenTo(actions.getMix_Dzzd_data, 'getMix_Dzzd_data');
 	},
 	getBigscreenSecondData () {
-    	$.ajax({
-    		async: false,
-    		type : "post",
-    		url : ROOF.Utils.projectName() + "/ems/bigscreen_backstage/SecondBackstageAction/get_bigscreen_second_data.action",
-    		//data: {pageNo:0,pageSize:100},
-    		datatype : 'json',
-    		success : function(d) {
-    			this.trigger({chartList:d.data.chartList});
-    			this.trigger({relList:d.data.relList});
-    			this.trigger({bigscreen_id:d.data.bigscreen.id});
-    			this.trigger({theme_id:d.data.theme.id});
-    			this.trigger({templates_id:d.data.templates.id});
-    			this.trigger({templates_path:d.data.templates.path});
-    		}.bind(this)
-    	});
-    },
-    saveBigscreenSecondData (data) {
-    	$.ajax({
-    		async: false,
-    		type: "post",
-    		url: ROOF.Utils.projectName() + "/ems/bigscreen_backstage/SecondBackstageAction/save.action",
-    		data: data,
-    		datatype : 'json',
-    		success : function(d) {
-    			alert(d.message);
-    			this.getBigscreenSecondData();
-    		}.bind(this)
-    	});
-    },
-    openAddModal(showFlag){
-    	this.trigger({showAddUserModal:showFlag});
-    },
-    setStateValue(value){
+		$.ajax({
+			async: false,
+			type : "post",
+			url : ROOF.Utils.projectName() + "/ems/bigscreen_backstage/SecondBackstageAction/get_bigscreen_second_data.action",
+			//data: {pageNo:0,pageSize:100},
+			datatype : 'json',
+			success : function(d) {
+				this.trigger({chartList:d.data.chartList});
+				this.trigger({relList:d.data.relList});
+				this.trigger({bigscreen_id:d.data.bigscreen.id});
+				this.trigger({theme_id:d.data.theme.id});
+				this.trigger({templates_id:d.data.templates.id});
+				this.trigger({templates_path:d.data.templates.path});
+			}.bind(this)
+		});
+	},
+	saveBigscreenSecondData (data) {
+		$.ajax({
+			async: false,
+			type: "post",
+			url: ROOF.Utils.projectName() + "/ems/bigscreen_backstage/SecondBackstageAction/save.action",
+			data: data,
+			datatype : 'json',
+			success : function(d) {
+				alert(d.message);
+				this.getBigscreenSecondData();
+			}.bind(this)
+		});
+	},
+	openAddModal(showFlag){
+		this.trigger({showAddUserModal:showFlag});
+	},
+	setStateValue(value){
 		this.trigger(value);
 	},
 	// 1.全省设备排名
@@ -91,7 +91,7 @@ export default Reflux.createStore({
 			}.bind(this)
 		});
 	},
-    // 3.定制终端
+	// 3.定制终端
 	getMix_Dzzd_data(){
 		$.ajax({
 			async: false,
